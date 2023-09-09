bc84723ac7818cf233f3c4845286d305c6540bc8
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-09-09 10:16:31 -0500

    Estructura del backend

---
+++ reto/innoHacks/src/main/java/com/innoHacks/innoHacks/InnoHacksApplication.java
@@ -0,0 +1,13 @@
+package com.innoHacks.innoHacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoHacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoHacksApplication.class, args);
+	}
+
+}
