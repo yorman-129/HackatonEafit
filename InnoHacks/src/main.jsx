4ffae9da8627f44e3eac8facdc01cb8c877728b0
Andrés Parra <pipe.parrab@gmail.com>
2023-09-09 13:58:20 -0500

    Configuración del Chakra.

--- InnoHacks/src/main.jsx
+++ InnoHacks/src/main.jsx
@@ -1,12 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router/AppRouter.jsx";
-import App from "./App.jsx";
 import "./index.scss";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AppRouter />
-  </BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </ChakraProvider>
 );
