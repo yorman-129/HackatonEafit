e383269704ce23834c46318859bd51dec59503ed
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-09-09 14:49:19 -0500

    Nav terminado

--- InnoHacks/src/pages/Layout/Layout.jsx
+++ InnoHacks/src/pages/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 import {Outlet, Link, useNavigate} from 'react-router-dom'
+import '../Layout/Layout.scss'
 
 const Layout = () => {
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
     // Cuando yo quiera mandar a otra ruta, uso navigate('/user/${id}/${nombre}')
   return (
     <>
-        <nav>
-            <ul>
-                <li> <Link to="/home">Home</Link></li>
-            </ul>
+        <nav className='nav-container'>
+            <a href='/'><img src="../../../public/favicon.ico" alt="" /></a>
+            <a className='nav-container__projects' href="/Projects">Projects</a>
         </nav>
         <div>Esta es la parte donde se va a compartir con los otros componentes</div>
         <Outlet/>
