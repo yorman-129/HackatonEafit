8383a98f82f561d617a95c60669a87ccc6155bc7
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-09-09 16:23:36 -0500

    Finished crud

--- reto/innoHacks/src/main/java/com/innoHacks/innoHacks/controller/projectController.java
+++ reto/innoHacks/src/main/java/com/innoHacks/innoHacks/controller/projectController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class projectController {
     @Autowired
     private ProjectService serviceProject;
@@ -23,17 +24,7 @@ public class projectController {
         serviceProject.saveProject(project);
     }
 
-<<<<<<< HEAD
-
-    @GetMapping("projects")
-    public List<Project> allProjects(){ return serviceProject.allProjects(); }
-
-    @GetMapping("/project/{id}")
-    public Project findProject(@PathVariable Long id){
-        return serviceProject.findProject(id);
-    }
-=======
-    @GetMapping("/projects/")
+    @GetMapping("/projects")
     public List<Project> allProjects(){ return serviceProject.allProjects(); }
 
     @GetMapping("/project/{id}")
@@ -41,6 +32,5 @@ public class projectController {
 
     @DeleteMapping("/project/{id}")
     public void deleteProject(@PathVariable("id") Long id){ serviceProject.deleteProject(id);}
->>>>>>> 1c94c4d4bd66828b5a4f26c3fa6efd34feffffaa
 
 }
