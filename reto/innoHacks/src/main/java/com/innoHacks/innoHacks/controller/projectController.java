7d6b9a14389579854de715fc0f06a3c48716d1d8
lvaleriaescobar <valeriamedinaes@gmail.com>
2023-09-09 13:24:37 -0500

    Project controller

---
+++ reto/innoHacks/src/main/java/com/innoHacks/innoHacks/controller/projectController.java
@@ -0,0 +1,27 @@
+package com.innoHacks.innoHacks.controller;
+
+import com.innoHacks.innoHacks.model.Project;
+import com.innoHacks.innoHacks.model.Task;
+import com.innoHacks.innoHacks.services.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class projectController {
+    @Autowired
+    private ProjectService serviceProject;
+
+    @PostMapping("/addProject")
+    public void saveProject(@RequestBody Project project){
+        serviceProject.saveProject(project);
+    }
+    @GetMapping
+    public List<Project> allProjects(){ return serviceProject.allProjects(); }
+
+}
